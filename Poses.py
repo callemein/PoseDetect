81d0b268e44bf24366f18f9179ee1ad9bb07d6a3
Timothy Callemein <timothy.callemein@kuleuven.be>
2017-10-17 09:49:30 +0200

    Add poses

---
+++ Poses.py
@@ -0,0 +1,23 @@
+from Pose import *
+
+
+class Poses:
+
+    def __init__(self, raw_pose_data, hand_threshold = 0.0, head_threshold=0.0):
+        self.raw_pose_data = raw_pose_data
+
+        self.hand_threshold = hand_threshold
+        self.head_threshold = head_threshold
+
+        self.poses = []
+
+        self.fetch_poses(raw_pose_data, hand_threshold, head_threshold)
+
+    def draw_all_poses(self, frame):
+        for p in self.poses:
+            p.draw_pose(frame)
+
+    def fetch_poses(self, raw_pose_data, hand_threshold, head_threshold):
+
+        for p in raw_pose_data['people']:
+            self.poses.append(Pose(p, hand_threshold, head_threshold))
