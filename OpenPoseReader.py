a9a7d9c1f1b35cfacd86e75e74577990c960b2a4
Timothy Callemein <callemein.timothy@gmail.com>
2018-09-14 16:22:12 +0200

    Add zipfile option

--- OpenPoseReader.py
+++ OpenPoseReader.py
@@ -1,5 +1,6 @@
 import json
 import os
+import zipfile
 
 __all__ = ["OpenPoseReader"]
 
@@ -20,11 +21,23 @@ class OpenPoseReader:
         self.current_file = None
         self.raw_data = {}
 
+
+        if path.endswith('.zip'):
+            try:
+                self.zfile = zipfile.ZipFile(path, 'r')
+                ret = zfile.testzip()
+                if ret is not None:
+                    print "%s bad zip file, error: %s" % file, ret
+            except zipfile.BadZipfile as ex:
+                print "%s no a zip file" % path
+
+    def load_file_by_frame_from_zip(self, frame):
+        self.load_zfile(self.prefix + str(frame).zfill(self.frame_zfill) + self.suffix + ".json")
+
     def load_file_by_frame(self, frame):
         self.load_file(self.openpose_path + self.prefix + str(frame).zfill(self.frame_zfill) + self.suffix + ".json")
 
     def load_file_by_name(self, filename):
-        print("TEST")
         self.load_file(self.openpose_path + str(os.path.splitext(filename)[0]) + self.suffix + ".json")
 
     def load_file(self, path):
@@ -32,3 +45,7 @@ class OpenPoseReader:
             self.current_file = path
             self.raw_data = json.load(json_data)
             json_data.close()
+
+    def load_zfile(self, path):
+        json_data = self.zfile.read(path) 
+        self.raw_data = json.loads(json_data)
